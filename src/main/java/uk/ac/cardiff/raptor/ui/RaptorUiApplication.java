af89d391c4874c08e276e059804b54c118aa9c3c
phil <smartp@cf.ac.uk>
2017-05-16 20:47:03 +0100

    basic setup

--- src/main/java/uk/ac/cardiff/raptor/ui/RaptorUiApplication.java
+++ src/main/java/uk/ac/cardiff/raptor/ui/RaptorUiApplication.java
@@ -2,38 +2,42 @@ package uk.ac.cardiff.raptor.ui;
 
 import javax.faces.webapp.FacesServlet;
 
-import org.eclipse.jdt.internal.compiler.batch.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import uk.ac.cardiff.raptor.ui.web.ConfigureJSFContextParameters;
-
 @Configuration
 @ComponentScan(basePackages = { "uk.ac.cardiff" })
 @EnableAutoConfiguration
 @EnableScheduling
-public class RaptorUiApplication extends SpringBootServletInitializer {
+public class RaptorUiApplication {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(RaptorUiApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-		return application.sources(new Class[] { Main.class, ConfigureJSFContextParameters.class });
-	}
-
+	/**
+	 * Register the {@link FacesServlet} within the current
+	 * {@link org.springframework.boot.web.servlet.ServletContextInitializer},
+	 * and hence with the v3 {@link javax.servlet.Servlet} provider. This maps
+	 * all URL patterns *.xhtml to the {@link FacesServlet}.
+	 *
+	 * @return a {@link ServletRegistrationBean} with registered
+	 *         {@link FacesServlet}
+	 */
 	@Bean
 	public ServletRegistrationBean servletRegistrationBean() {
 		final FacesServlet servlet = new FacesServlet();
-		final ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.html");
+
+		final ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet,
+				new String[] { "*.xhtml" });
+		servletRegistrationBean.setName("FacesServlet");
+		servletRegistrationBean.setLoadOnStartup(1);
+
 		return servletRegistrationBean;
 	}
 
